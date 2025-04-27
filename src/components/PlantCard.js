2630a88b5a2ae60ff22c64bff30f0228b1f77338
Priscillah Njai <priscillahnjaii@gmail.com>
2025-04-27 21:25:41 +0300

    Initial commit

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({ plant, markAsSoldOut }) {
+  // Check if inStock is undefined, assume it's true by default
+  const inStock = plant.inStock !== undefined ? plant.inStock : true;
+  
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      {inStock ? (
+        <button 
+          className="primary" 
+          onClick={() => markAsSoldOut(plant.id)}
+        >
+          In Stock
+        </button>
       ) : (
         <button>Out of Stock</button>
       )}
